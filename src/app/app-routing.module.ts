3b869fce484a6bd0b351a592d731e05458400540
Khalid hasan <khalidhasan024@gmail.com>
2020-08-24 10:39:30 +0600

    Angular config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "product", loadChildren: () => import("./product-detail/product-detail.module").then(m => m.ProductDetailModule)
+  },
+  {
+    path: "cart", loadChildren: () => import("./cart/cart.module").then(m => m.CartModule)
+  },
+  {
+    path: "", pathMatch: "full", loadChildren: () => import("./home/home.module").then(m => m.HomeModule) 
+  },
+  {
+    path: "**", component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { ProductService } from './core/product.service'
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
